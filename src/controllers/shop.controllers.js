5ad82ff7cd0acaee7a7277f4387a291c6e036d88
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-11-24 00:40:44 +0530

    implement ACID transactions for buy endpoint
    - Integrated mongoose sessions to handle concurrency.
    - Wrapped stock deduction and order creation in a single transaction.
    - Fixed race condition causing overselling during high load.

--- src/controllers/shop.controllers.js
+++ src/controllers/shop.controllers.js
@@ -4,6 +4,7 @@ import { AsyncHandler } from "../utils/async-handler.js";
 
 import { Product } from "../models/product.models.js";
 import { Order } from "../models/order.models.js";
+import mongoose from "mongoose";
 
 const getProducts = AsyncHandler(async (req, res) => {
   const products = await Product.find();
@@ -16,30 +17,40 @@ const buyProduct = AsyncHandler(async (req, res) => {
   const userId = req.user.id;
   const { productId } = req.body;
 
-  const product = await Product.findById(productId);
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-  const existingOrder = await Order.findOne({ userId: userId, productId: productId });
-
+  const existingOrder = await Order.findOne({ userId, productId }).session(session);
   if (existingOrder) {
+    await session.abortTransaction();
+    session.endSession();
     return res.status(400).json(new ApiError(400, "You have already purchased an iPhone."));
   }
-
+  
+  const product = await Product.findById(productId).session(session);
   if (!product) {
+    await session.abortTransaction();
+    session.endSession();
     return res.status(400).json(new ApiError(400, "Product not found."));
   }
 
   if (product.stock <= 0) {
+    await session.abortTransaction();
+    session.endSession();
     return res.status(400).json(new ApiError(400, "Out of stock!"));
   }
 
   product.stock = product.stock - 1;
-  await product.save();
+  await product.save({ session });
 
   const order = new Order({
     userId: userId,
     productId: productId,
   });
-  await order.save();
+  await order.save({ session });
+
+  await session.commitTransaction();
+  session.endSession();
 
   res
     .status(200)
